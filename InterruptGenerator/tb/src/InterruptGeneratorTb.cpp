e0fb8ad2f90211fe9428968af558b8d4a9e8e59e
Jayshree Ambekar <jayu.ambekar@gmail.com>
2018-07-19 21:55:44 +0530

    testcaes added

--- InterruptGenerator/tb/src/InterruptGeneratorTb.cpp
+++ InterruptGenerator/tb/src/InterruptGeneratorTb.cpp
@@ -5,16 +5,21 @@ InterruptGeneratorTb :: InterruptGeneratorTb(sc_core::sc_module_name module_name
 	   ,initiatorSocket("initiatorSocket")
 	   ,interruptsIn("interruptsIn", 10)
 {
+	SC_THREAD(test);
 
+	SC_METHOD(monitor);
 }
 
 void InterruptGeneratorTb :: test()
 {
+	wait(1, sc_core::SC_NS);
+	std::cout << "[ " << sc_core::sc_time_stamp() << " ] " << " data_out.read() " << std::endl;
 
+	wait(10, sc_core::SC_NS);
 }
 
 void InterruptGeneratorTb :: monitor()
 {
-
+	std::cout << "[ " << sc_core::sc_time_stamp() << " ] Interrupt asserted : Intr0 " << std::endl;
 }
 
